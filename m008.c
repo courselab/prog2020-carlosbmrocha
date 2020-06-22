71fb089cf82b4a392ebbe62f05f8a2f8d043a1de
Carlos <carlosbmrocha@usp.br>
2020-06-22 15:39:42 -0300

    m008

--- m008.c
+++ m008.c
@@ -1,16 +1,16 @@
 /* m008.c - Lastname-Names.
 
    This program reads the name of a person from the standard input
-   in the form 'First-name middle-names last-name' and outputs the 
+   in the form 'First-name middle-names last-name' and outputs the
    name in the form 'LAST-NAME, first-name middle-names'.
-   The name should be entered using only ASCII characters (26 letter 
+   The name should be entered using only ASCII characters (26 letter
    of latim alphabet, no diacrictics).
 
 
    E.g.
 
       command line    : m008
-      standard input  : John Little Bill Doe 
+      standard input  : John Little Bill Doe
       expected output : DOE, John Little Bill
 
    Directions:
@@ -22,14 +22,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 256
 
-/* Converts the string 's' from first-middle-last name 
+/* Converts the string 's' from first-middle-last name
    form into LAST-first-middle form. */
 
-void lastname (char *s)
+void lastname (char *c)
 {
+    char a[MAX];
+    char b[MAX];
+    int i, x, j, n;
+
+    strcpy(a, c);
+    n = strlen(a);
+
+    i = 0;
+    while ( a[i] != 0)
+    {
+        if (a[i] == ' ')
+            x = i;
+        i++;
+    }
+    for (j = x + 1; j < n ; j++)
+    {
+        if ( (a[j] <=122) && (a[j] >= 97) )
+            b[j - x - 1]= a[j] - 32;
+            else
+            b[j - x - 1] = a[j];
+    }
+
+    b[n - x - 1]= ',';
+    b[n - x ]= ' ';
+
+    for (j= 0; j < x; j++)
+     b[n - x + 1 + j]=a[j];
+
+     b[n + 1 ] = 0;
+
+
+    strcpy(c,b);
+
 }
 
 
@@ -42,10 +76,10 @@ int main (int argc, char **argv)
   char name[MAX];
 
   fgets (name, MAX-1, stdin);
-  
+
   lastname (name);
 
   printf ("%s\n", name);
-  
+
   return 0;
 }
